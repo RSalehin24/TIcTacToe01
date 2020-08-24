c6e7b6f298f89bf2451cf6cc0df76e89c8c7693e
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2020-08-25 00:53:33 +0600

    #1 changes order of the functions to make it easier to understand.

--- solutions/48-reyan/src/GameStage.java
+++ solutions/48-reyan/src/GameStage.java
@@ -37,6 +37,10 @@ public class GameStage {
         stageOfGame.show();
     }
 
+    private Scene createGameScene(){
+        return new Scene(createGamePane());
+    }
+
     private Pane createGamePane(){
         paneOfGame = new Pane();
         paneOfGame.setPrefSize(600, 380);
@@ -50,10 +54,6 @@ public class GameStage {
 
         paneOfGame.getChildren().addAll(tileGroup, lineGroup, textTheme, buttonGroup, radioButtonGroup);
         return paneOfGame;
-    }
-
-    private Scene createGameScene(){
-        return new Scene(createGamePane());
     }
 
     protected void createTileGroup(){
@@ -71,12 +71,6 @@ public class GameStage {
         }
     }
 
-    protected Line drawLine(double x1, double y1, double x2, double y2){
-        Line line = new Line(x1, y1, x2, y2);
-        line.setStrokeWidth(9);
-        return line;
-    }
-
     private void createLineGroup(){
         int l=0;
         lines[l] = drawLine(158, 30, 158, 350);
@@ -90,6 +84,12 @@ public class GameStage {
         lines[l] = drawLine(400, 30, 400, 348);
 
         lineGroup.getChildren().addAll(lines[0], lines[1], lines[2], lines[3], lines[4]);
+    }
+
+    protected Line drawLine(double x1, double y1, double x2, double y2){
+        Line line = new Line(x1, y1, x2, y2);
+        line.setStrokeWidth(9);
+        return line;
     }
 
     protected void thingsToChangeForTheme(Pane gamePane, Color colorOfPane, Color colorOfLine){
