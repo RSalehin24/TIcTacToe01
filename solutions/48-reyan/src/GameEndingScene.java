6eed1ba164067d10a4c4e31946f7f893836d4b31
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2020-08-24 16:17:10 +0600

    Revert "created function to determine string for the label to make the  makeGameOverScene makeGameOverScene easy to understand,  added unit testing to test determineString function #1"
    This reverts commit 963cc383

--- solutions/48-reyan/src/GameEndingScene.java
+++ solutions/48-reyan/src/GameEndingScene.java
@@ -9,43 +9,40 @@ public class GameEndingScene {
 
     public Scene makeGameOverScene(int winPlayerIndicator) {
 
-        String stringForLabelOne = determineString(winPlayerIndicator, "Yahoo!!!", "Sorry!", "Hmm!");
-        String stringForLabelTwo = determineString(winPlayerIndicator, "You have won the game!!","You have lost the game", "Game Has Drawn");
+        String stringForLabelOne, stringForLabelTwo;
+        if (winPlayerIndicator == 0) {
+            stringForLabelOne = "Yahoo!!!";
+            stringForLabelTwo = "You have won the game!!";
+        } else if(winPlayerIndicator == 1) {
+            stringForLabelOne = "Sorry!";
+            stringForLabelTwo = "You have lost the game";
+        } else {
+            stringForLabelOne = "Hmm!";
+            stringForLabelTwo = "Game Has Drawn";
+        }
 
         Label labelOne = createLabel(stringForLabelOne,"#ff0000");
         Label labelTwo = createLabel(stringForLabelTwo, "#228b22");
 
-        VBox vBox = creatVBox(labelOne, labelTwo);
+        VBox vBox = creatVBox(labelOne,labelTwo);
 
         Scene scene = new Scene(vBox, 420, 380);
 
         return scene;
     }
 
-    protected Label createLabel(String string, String color){
+    private Label createLabel(String string, String color){
         Label label = new Label(string);
         label.setFont(new Font("Arial", 30));
         label.setTextFill(Color.web(color, 1.0));
         return label;
     }
 
-    protected VBox creatVBox(Label labelOne, Label labelTwo){
+    private VBox creatVBox(Label labelOne, Label labelTwo){
         VBox vBox = new VBox(5);
         vBox.setAlignment(Pos.CENTER);
         vBox.getChildren().addAll(labelOne, labelTwo);
 
         return vBox;
-    }
-
-    protected String determineString(int winPlayerIndicator, String setStringOne, String setStringTwo, String setStringThree){
-        String string;
-        if (winPlayerIndicator == 0) {
-            string = setStringOne;
-        } else if(winPlayerIndicator == 1) {
-            string = setStringTwo;
-        } else {
-            string = setStringThree;
-        }
-        return string;
     }
 }
