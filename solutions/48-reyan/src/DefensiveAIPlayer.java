b35ca8c94df58aa39d9384a08c3fe641d0639f86
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2020-09-14 16:11:36 +0600

    #1 changed the function name to understand its work properly.

--- solutions/48-reyan/src/DefensiveAIPlayer.java
+++ solutions/48-reyan/src/DefensiveAIPlayer.java
@@ -28,41 +28,41 @@ public class DefensiveAIPlayer implements AIPlayer{
 
     private int checkRowsTiles(int tileNo, boolean[][] occupiedTiles){
         for (int i=0; i<9; i += 3) {
-            tileNo = checkTheTiles(tileNo, occupiedTiles, i, i+1, i+2);
-            tileNo = checkTheTiles(tileNo, occupiedTiles, i, i+2, i+1);
-            tileNo = checkTheTiles(tileNo, occupiedTiles, i+1,i+1, i);
+            tileNo = checkIfTwoTilesHaveMatched(tileNo, occupiedTiles, i, i+1, i+2);
+            tileNo = checkIfTwoTilesHaveMatched(tileNo, occupiedTiles, i, i+2, i+1);
+            tileNo = checkIfTwoTilesHaveMatched(tileNo, occupiedTiles, i+1,i+1, i);
         }
         return tileNo;
     }
 
     private int checkColumnsTiles(int tileNo, boolean[][] occupiedTiles){
         for(int i=0; i<3; i++){
-            tileNo = checkTheTiles(tileNo, occupiedTiles, i, i+3, i+6);
-            tileNo = checkTheTiles(tileNo, occupiedTiles, i, i+6, i+3);
-            tileNo = checkTheTiles(tileNo, occupiedTiles, i+3,i+6, i);
+            tileNo = checkIfTwoTilesHaveMatched(tileNo, occupiedTiles, i, i+3, i+6);
+            tileNo = checkIfTwoTilesHaveMatched(tileNo, occupiedTiles, i, i+6, i+3);
+            tileNo = checkIfTwoTilesHaveMatched(tileNo, occupiedTiles, i+3,i+6, i);
         }
         return tileNo;
     }
 
     private int checkMainDiagonalTiles(int tileNo, boolean[][] occupiedTiles){
         for(int i=0; i<3; i++){
-            tileNo = checkTheTiles(tileNo, occupiedTiles, 0, 4, 8);
-            tileNo = checkTheTiles(tileNo, occupiedTiles, 0, 8, 4);
-            tileNo = checkTheTiles(tileNo, occupiedTiles, 4,8, 0);
+            tileNo = checkIfTwoTilesHaveMatched(tileNo, occupiedTiles, 0, 4, 8);
+            tileNo = checkIfTwoTilesHaveMatched(tileNo, occupiedTiles, 0, 8, 4);
+            tileNo = checkIfTwoTilesHaveMatched(tileNo, occupiedTiles, 4,8, 0);
         }
         return tileNo;
     }
 
     private int checkAuxiliaryDiagonalTiles(int tileNo, boolean[][] occupiedTiles){
         for(int i=0; i<3; i++){
-            tileNo = checkTheTiles(tileNo, occupiedTiles, 2, 4, 6);
-            tileNo = checkTheTiles(tileNo, occupiedTiles, 2, 6, 4);
-            tileNo = checkTheTiles(tileNo, occupiedTiles, 4,6, 2);
+            tileNo = checkIfTwoTilesHaveMatched(tileNo, occupiedTiles, 2, 4, 6);
+            tileNo = checkIfTwoTilesHaveMatched(tileNo, occupiedTiles, 2, 6, 4);
+            tileNo = checkIfTwoTilesHaveMatched(tileNo, occupiedTiles, 4,6, 2);
         }
         return tileNo;
     }
 
-    private int checkTheTiles(int tileNo, boolean[][] occupiedTiles, int tileOne, int tileTwo, int tileThree){
+    private int checkIfTwoTilesHaveMatched(int tileNo, boolean[][] occupiedTiles, int tileOne, int tileTwo, int tileThree){
         if(tileNo == -1) {
             if (occupiedTiles[0][tileOne] && occupiedTiles[0][tileTwo]) {
                 if (!(occupiedTiles[0][tileThree] || occupiedTiles[1][tileThree])) tileNo = tileThree;
